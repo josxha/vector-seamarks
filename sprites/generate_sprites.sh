255e8169af6127bbb4cb92e9e659e907944214da
Joscha <34318751+josxha@users.noreply.github.com>
2024-06-22 11:33:02 +0200

    make generate_sriptes.sh runnable

--- sprites/generate_sprites.sh
+++ sprites/generate_sprites.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+# Generate sprites, install spreet with `brew install flother/taps/spreet`
+
 spreet --unique --recursive svg seamarks-sprites
 spreet --unique --recursive --retina svg seamarks-sprites@2x
