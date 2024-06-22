ac6a4cbffe4c59e9bcc8018ca02c4a4d61133d76
Joscha <34318751+josxha@users.noreply.github.com>
2024-06-22 12:39:20 +0200

    update and enable CI

--- sprites/generate_sprites.sh
+++ sprites/generate_sprites.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 
-# Generate sprites, install spreet with `brew install flother/taps/spreet`
+# Copy all SVG files to a temporary flat directory
+mkdir -p tmp/svg
+find svg -type f -exec cp {} tmp/svg/ \;
 
-spreet --unique --recursive svg seamarks-sprites
-spreet --unique --recursive --retina svg seamarks-sprites@2x
+# Generate sprites, install spreet with `brew install flother/taps/spreet`
+spreet --unique --recursive tmp/svg seamarks-sprites
+spreet --unique --recursive --retina tmp/svg seamarks-sprites@2x
+
+# Add the sdf field when the sprite name ends with _sdf
+temp_file=$(mktemp)
+jq 'walk(if type == "object" then with_entries(
+    if (.key | endswith("_sdf")) then .value |= . + {"sdf": true} else . end
+) else . end)' "seamarks-sprites.json" > $temp_file
+mv $temp_file "seamarks-sprites.json"
+
+jq 'walk(if type == "object" then with_entries(
+    if (.key | endswith("_sdf")) then .value |= . + {"sdf": true} else . end
+) else . end)' "seamarks-sprites@2x.json" > $temp_file
+mv $temp_file "seamarks-sprites@2x.json"
